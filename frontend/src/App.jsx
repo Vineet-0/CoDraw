f1789fa3caebf45888b5da5c542738b62b321e3e
Vineet-0 <vineet351016@gmail.com>
2023-10-02 23:23:16 +0530

    first Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'; // Import React
+import './App.css';
+
+// Use the correct syntax for defining a functional component
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
